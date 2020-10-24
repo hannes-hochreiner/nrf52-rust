385904a39a6db24a44f52a52fca34181449129a4
Hannes Hochreiner <hannes@hochreiner.net>
2020-10-24 21:56:24 +0200

    added uart

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use nrf52810_hal as hal;
 use nrf52810_hal::gpio::Level;
 use nrf52810_hal::prelude::OutputPin;
 use nrf52810_hal::prelude::_embedded_hal_blocking_delay_DelayMs;
+use core::fmt::Write;
 
 #[entry]
 fn main() -> ! {
@@ -20,8 +21,15 @@ fn main() -> ! {
     let p = hal::pac::Peripherals::take().unwrap();
     let cp = hal::pac::CorePeripherals::take().unwrap();
     let port0 = hal::gpio::p0::Parts::new(p.P0);
-    let mut led = port0.p0_18.into_push_pull_output(Level::Low);
+    let mut led = port0.p0_14.into_push_pull_output(Level::Low);
     let mut delay = hal::delay::Delay::new(cp.SYST);
+    let pins = hal::uarte::Pins {
+        rxd: port0.p0_16.into_floating_input().degrade(),
+        txd: port0.p0_18.into_push_pull_output(Level::Low).degrade(),
+        cts: None,
+        rts: None
+    };
+    let mut uart = hal::uarte::Uarte::new(p.UARTE0, pins, hal::uarte::Parity::EXCLUDED, hal::uarte::Baudrate::BAUD115200);
 
     loop {
         // your code goes here
@@ -29,5 +37,6 @@ fn main() -> ! {
         delay.delay_ms(1000u32);
         led.set_low().unwrap();
         delay.delay_ms(1000u32);
+        uart.write_str(&"test\n").unwrap();
     }
 }
